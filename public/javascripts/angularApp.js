141dd23d1fb58d5dde938f7ac501241fcda91371
Nin7aStar <xiaoguangyu361@gmail.com>
2017-02-17 23:14:48 +0800

    Creating the Login and Register pages

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -1,54 +1,54 @@
-var app = angular.module('flapperNews', ['ui.router'])
+var app = angular.module('flapperNews', ['ui.router']);
 
 app.config([
-'$stateProvider',
-'$urlRouterProvider',
-function($stateProvider, $urlRouterProvider) {
-
-  $stateProvider
-    .state('login', {
-        url: '/login',
-        templateUrl: '/login.html',
-        controller: AuthCtrl,
-        onEnter: ['$state', 'auth', function($state, auth) {
-            if (auth.isLoggedIn()) {
-                $state.go('home');
-            }
-        }]
-    })
-    .state('register', {
-        url: '/register',
-        templateUrl: '/register.html',
-        controller: AuthCtrl,
-        onEnter: ['$state', 'auth', function ($state, auth) {
-            if (auth.isLoggedIn()) {
-                $state.go('home');
-            }
-        }]
-    })
-    .state('home', {
-      url: '/home',
-      templateUrl: '/home.html',
-      controller: 'MainCtrl',
-      resolve: {
-          postPromise: ['posts', function (posts) {
-              return posts.getAll();
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+      $stateProvider
+        .state('home', {
+          url: '/home',
+          templateUrl: '/home.html',
+          controller: 'MainCtrl',
+          resolve: {
+              postPromise: ['posts', function (posts) {
+                  return posts.getAll();
+              }]
+          }
+        })
+        .state('posts', {
+          url: '/posts/{id}',
+          templateUrl: '/posts.html',
+          controller: 'PostsCtrl',
+          resolve: {
+              post: ['$stateParams', 'posts', function($stateParams, posts){
+                return posts.get($stateParams.id);
+              }]
+          }
+        })
+        .state('login', {
+          url: '/login',
+          templateUrl: '/login.html',
+          controller: 'AuthCtrl',
+          onEnter: ['$state', 'auth', function($state, auth) {
+              if (auth.isLoggedIn()) {
+                  $state.go('home');
+              }
           }]
-      }
-    })
-    .state('posts', {
-      url: '/posts/{id}',
-      templateUrl: '/posts.html',
-      controller: 'PostsCtrl',
-      resolve: {
-          post: ['$stateParams', 'posts', function($stateParams, posts){
-            return posts.get($stateParams.id);
+        })
+        .state('register', {
+          url: '/register',
+          templateUrl: '/register.html',
+          controller: 'AuthCtrl',
+          onEnter: ['$state', 'auth', function ($state, auth) {
+              if (auth.isLoggedIn()) {
+                  $state.go('home');
+              }
           }]
-      }
-    });
+      });
 
-  $urlRouterProvider.otherwise('home');
-}]);
+      $urlRouterProvider.otherwise('home');
+    }
+]);
 
 // Angular service(factory)
 app.factory('posts', ['$http', 'auth', function($http, auth) {
@@ -162,9 +162,10 @@ app.factory('auth', ['$http', '$window', function ($http, $window) {
 
 // Main controller
 app.controller('MainCtrl', [
-  '$scope',
-  'posts',
-  function ($scope, posts) {
+    '$scope',
+    'posts',
+    'auth',
+  function ($scope, posts, auth) {
     $scope.isLoggedIn = auth.isLoggedIn;
 
     $scope.posts = posts.posts;
@@ -189,7 +190,8 @@ app.controller('PostsCtrl', [
     '$scope',
     'posts',
     'post',
-    function($scope, posts, post) {
+    'auth',
+    function($scope, posts, post, auth) {
         $scope.isLoggedIn = auth.isLoggedIn;
 
         // Fetch post object from state parameter
@@ -237,7 +239,7 @@ app.controller('AuthCtrl', [
             });
         };
     }
-])
+]);
 
 // Navbar controller
 app.controller('NavCtrl', [
